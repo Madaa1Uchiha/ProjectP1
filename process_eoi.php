2c94a2c3931e7b9237086166e587e94a8fd94443
Cole <104558037@student.swin.edu.au>
2025-05-20 21:13:52 +1000

    added query to add to eoi table and get eio num

--- process_eoi.php
+++ process_eoi.php
@@ -20,4 +20,19 @@ $dob = trim($_POST['date-of-birth']);
 $gender = trim($_POST['gender']);
 $willingtomove = trim($_POST['willing-to-move']);
 $otherskills = trim($_POST['other-skills']);
+
+$query = "INSERT INTO eoi (Job_Reference_number, first_name, middle_name, last_name, 
+skills1, skills2, skills3, email_address, phone_number, state, address, suburb/town, date_of_birth, gender,
+willing-to-move, other-skills) VALUES ('$position', '$firstname', '$middlename', '$lastname'
+, '$skills1', '$skills2', '$skills3', '$email', '$phonenumber', '$state'
+, '$address', '$suburb', '$dob', '$gender', '$willingtomove', '$otherskills')";
+$result = mysqli_query($conn, $query);
+
+if ($result) {
+    echo "Expression of interest recieved\nYour unique EOI number is";
+    echo $query = "SELECT EOInumber FROM `eoi` WHERE email = '$email' ";
+}
+else {
+    echo "Application failed. Please try again.";
+  }
 ?>
