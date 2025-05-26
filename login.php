842edfd2a46de771119df9fccbbfa60b1c47bb95
105828391 <105828391@student.swin.edu.au>
2025-05-26 12:06:35 +1000

    updated login page so session ressets if the browser is closed

--- login.php
+++ login.php
@@ -1,4 +1,11 @@
-
+<?php
+session_set_cookie_params(0);
+session_start();
+if (isset($_SESSION['username']))
+{
+    header("Location:manage.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
