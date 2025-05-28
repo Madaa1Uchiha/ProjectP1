bc81ac2c13e1bbd7f7b4976ef678b685690e9f3d
Cole <104558037@student.swin.edu.au>
2025-05-28 19:01:09 +1000

    made it so that reference num can not be chosen

--- apply.php
+++ apply.php
@@ -28,7 +28,7 @@ $page_title = 'LSCL Apply Page';
             echo ('<legend>Position</legend>'),
             ('<label for="position">Job Reference Number</label>'),
             ('<select name="position" id="position" required>'),
-                ('<option> Reference Number</option>'),
+                ('<option selected disabled> Reference Number</option>'),
                 ('<option value="FR432">FR432</option>'),
                 ('<option value="DS456">DS456</option>'),
             ('</select>');    
